6680d57571275ca075225cee25e7e6c46d31160c
Korey Rodi <rodikorey@gmail.com>
2025-04-10 17:15:46 -0400

    project.py

--- project.py
+++ project.py
@@ -1,3 +1,34 @@
 #imports
-import pywhatkit as pwk
+import pywhatkit
+# add regex to check phone numbers more effectively
 import tkinter as tk
+
+def checkNumberValid(phoneNumber):
+    if len(phoneNumber) == 14:
+        print("Valid Number")
+        return True
+    else:
+        print("Invalid number or format. Please input a new number")
+        return False
+
+def checkMessage(textMessage):
+    if len(textMessage) >= 1:
+        print("Valid Message")
+        return True
+    else:
+        print("Invalid message. Please input a new message")
+        return False
+
+def main():
+    phoneNumber = input("Enter phone number +1###-###-####: ")
+    while checkNumberValid(phoneNumber) == False:
+        phoneNumber = input("Enter phone number +1###-###-####: ")
+    phoneNumber = phoneNumber.replace("-", "")
+    textMessage = input("enter your message: ")
+    while checkMessage(textMessage) == False:
+        textMessage = input("enter your message: ")
+    pywhatkit.sendwhatmsg_instantly(phoneNumber, textMessage,5)
+
+
+if __name__ == "__main__":
+    main()
