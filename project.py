7ad0c77151de186f89d6900855dde72f93cb60fc
Korey Rodi <rodikorey@gmail.com>
2025-04-11 10:36:46 -0400

    project.py

--- project.py
+++ project.py
@@ -1,24 +1,38 @@
 import pywhatkit
 import tkinter as tk
 # import regex to check phone numbers more effectively
-# import pynput to automate enter when chat doesnt send
-# import time to allow enough time to enter next window
 
 def checkNumberValid(phoneNumber):
     if len(phoneNumber) == 14:
-        print("Valid Number")
+        #print("Valid Number")
         return True
     else:
-        print("Invalid number or format. Please input a new number")
+        #print("Invalid number or format. Please input a new number")
         return False
 
 def checkMessage(textMessage):
     if len(textMessage) >= 1:
-        print("Valid Message")
+        #print("Valid Message")
         return True
     else:
-        print("Invalid message. Please input a new message")
+        #print("Invalid message. Please input a new message")
         return False
+    
+def MyClick(phoneNumberField,textMessageField):
+    phoneNumber = phoneNumberField.get()
+    textMessage = textMessageField.get()
+    if checkNumberValid(phoneNumber) and checkMessage(textMessage) == True:
+        sendingMessage(phoneNumber,textMessage)
+    else:
+        textMessageField.delete(0,tk.END)
+        phoneNumberField.delete(0,tk.END)
+        textMessageField.insert(0,"Invalid Message and or Number")
+        phoneNumberField.insert(0,"Invalid Message and or Number")
+
+
+def sendingMessage(phoneNumberFormat,textMessageFormat):
+    pywhatkit.sendwhatmsg_instantly(phoneNumberFormat,textMessageFormat)
+
 
 def main():
     root = tk.Tk()
@@ -36,19 +50,9 @@ def main():
     textMessageField = tk.Entry(root, width=400, background="white",font=("arial",16),foreground="black")
     textMessageField.pack()
     textMessageField.insert(0,"Hello,John Doe!")
-    sendButton = tk.Button(root, text="Send",width=400)
+    sendButton = tk.Button(root, text="Send",width=400,command=lambda: MyClick(phoneNumberField, textMessageField))
     sendButton.pack()
     root.mainloop()
-    '''
-    phoneNumber = input("Enter phone number +1###-###-####: ")
-    while checkNumberValid(phoneNumber) == False:
-        phoneNumber = input("Enter phone number +1###-###-####: ")
-    phoneNumber = phoneNumber.replace("-", "")
-    textMessage = input("enter your message: ")
-    while checkMessage(textMessage) == False:
-        textMessage = input("enter your message: ")
-    pywhatkit.sendwhatmsg_instantly(phoneNumber, textMessage)
-    '''
 
 if __name__ == "__main__":
     main()
