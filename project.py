bf12db60ab1d18bd64953568773010da57c7f9df
Korey Rodi <rodikorey@gmail.com>
2025-04-27 12:34:37 -0400

    project.py

--- project.py
+++ project.py
@@ -1,46 +1,47 @@
 import pywhatkit
 import tkinter as tk
-#import tkinter.filedialog
+from tkinter import messagebox
 import re
 
 def checkNumberValid(phoneNumber):
     pattern = r"\+\d{4}-\d{3}-\d{4}"
     if re.fullmatch(pattern,phoneNumber):
-        #print(re.fullmatch)
         return True
     else:
         return False
     
 def checkMessage(textMessage):
     if len(textMessage) >= 1:
-        #print("Valid Message")
         return True
     else:
-        #print("Invalid message. Please input a new message")
         return False
+def clearFields(phoneNumberField,textMessageField):
+    phoneNumberField.delete(0,tk.END)
+    textMessageField.delete(0,tk.END)
     
 def MyClick(phoneNumberField,textMessageField):
     phoneNumber = phoneNumberField.get()
     textMessage = textMessageField.get()
     if checkNumberValid(phoneNumber) and checkMessage(textMessage) == True:
         sendingMessage(phoneNumber,textMessage)
-    else:
-        textMessageField.delete(0,tk.END)
+    if checkNumberValid(phoneNumber) == False:
         phoneNumberField.delete(0,tk.END)
-        textMessageField.insert(0,"Invalid Message and or Number")
-        phoneNumberField.insert(0,"Invalid Message and or Number")
+        phoneNumberField.insert(0,"Invalid Number")
+        messagebox.showwarning("Warning", "Please make sure to input valid Phone Number")
+    if checkMessage(textMessage) == False:
+        textMessageField.delete(0,tk.END)
+        textMessageField.insert(0,"Invalid Message")
+        messagebox.showwarning("Warning", "Please make sure to input valid text message")
 
 def sendingMessage(phoneNumberFormat,textMessageFormat):
     pywhatkit.sendwhatmsg_instantly(phoneNumberFormat,textMessageFormat)
 
-#def sendingMessageandImage(phoneNumberFormat,testMessageFormat,imageFormat):
-#    pywhatkit.sendwhats_image(receiver: str, img_path: str, caption: str = "")
-
 def main():
     root = tk.Tk()
     root.title("WhatsApp Messenger GUI")
-    root.geometry("400x160")
-    #root.geometry("400x225")
+    root.geometry("400x180")
+    messagebox.showinfo("information", "this is an extension of your whatsApp web account" \
+    " please make sure you have an active account, and all input fields are correctly entered")
     root.resizable(True, False)
     root.configure(bg="black")
     phoneNumberPrompt = tk.Label(root,text="Enter Phone Number Below",
@@ -50,7 +51,7 @@ def main():
                                 font=("arial",16),
                                 foreground="black",borderwidth=5,
                                 justify="center")
-    phoneNumberField.pack(fill="x")
+    phoneNumberField.pack(fill="x",expand=True)
     phoneNumberField.insert(0,"+1###-###-####")
     textMessagePrompt = tk.Label(root,text="Enter Your Message Below",
                                  background="black",font=("Lato",20))
@@ -58,20 +59,14 @@ def main():
     textMessageField = tk.Entry(root,background="white",
                                 font=("arial",16),foreground="black",
                                 borderwidth=5,justify="center")
-    textMessageField.pack(fill="x")
+    textMessageField.pack(fill="x", expand=True)
     textMessageField.insert(0,"Hello,John Doe!")
-    '''
-    imagePrompt = tk.Label(root,text="Attach Your Image Below", background="black",font=("Lato",20))
-    imagePrompt.pack(fill="x")
-    imagePromptField = tk.Entry(root,background="white",
-                                font=("arial",16),foreground="black",
-                                borderwidth=5, justify="center")
-    imagePromptField.pack(fill="x")
-    imagePromptField.insert(0,"Enter File Directory (optional)")
-    '''
+    clearButton = tk.Button(root, text = "Clear all Fields",
+                            command=lambda: clearFields(phoneNumberField,textMessageField))
+    clearButton.pack(fill="x", expand=True)
     sendButton = tk.Button(root, text="Send",
                            command=lambda: MyClick(phoneNumberField, textMessageField))
-    sendButton.pack(fill="x")
+    sendButton.pack(fill="x", expand=True)
     root.mainloop()
 
 if __name__ == "__main__":
