fb44c8a0f47ec3150e013bc75ad3b41caf8fbe13
Korey Rodi <rodikorey@gmail.com>
2025-04-27 14:10:37 -0400

    project.py

--- project.py
+++ project.py
@@ -18,36 +18,44 @@ def checkMessage(textMessage):
 def clearFields(phoneNumberField,textMessageField):
     phoneNumberField.delete(0,tk.END)
     textMessageField.delete(0,tk.END)
+    phoneNumberField.config(highlightbackground="black", highlightthickness=1)
+    textMessageField.config(highlightbackground="black", highlightthickness=1)
     
 def MyClick(phoneNumberField,textMessageField):
     phoneNumber = phoneNumberField.get()
     textMessage = textMessageField.get()
     if checkNumberValid(phoneNumber) and checkMessage(textMessage) == True:
-        sendingMessage(phoneNumber,textMessage)
+        sendingMessage(phoneNumber,textMessage,phoneNumberField,textMessageField)
     elif checkNumberValid(phoneNumber) == False and checkMessage(textMessage) == False:
         phoneNumberField.delete(0,tk.END)
         textMessageField.delete(0,tk.END)
+        phoneNumberField.config(highlightbackground="#cc0606",highlightthickness=1)
+        textMessageField.config(highlightbackground="#cc0606",highlightthickness=1)
         phoneNumberField.insert(0,"Invalid Number")
         textMessageField.insert(0,"Invalid Message")
-        messagebox.showerror("Error", "Please make sure to input valid Phone Number and Message")
+        messagebox.showerror("Error", "Please make sure to input a valid Phone Number and Message")
     elif checkNumberValid(phoneNumber) == False:
         phoneNumberField.delete(0,tk.END)
+        phoneNumberField.config(highlightbackground="#cc0606",highlightthickness=1)
         phoneNumberField.insert(0,"Invalid Number")
         messagebox.showerror("Error", "Please make sure to input valid Phone Number")
     elif checkMessage(textMessage) == False:
         textMessageField.delete(0,tk.END)
+        textMessageField.config(highlightbackground="#cc0606",highlightthickness=1)
         textMessageField.insert(0,"Invalid Message")
         messagebox.showerror("Error", "Please make sure to input valid text message")
 
-def sendingMessage(phoneNumberFormat,textMessageFormat):
+def sendingMessage(phoneNumberFormat,textMessageFormat,phoneNumberField,textMessageField):
+    phoneNumberField.config(highlightbackground="#146604",highlightthickness=3)
+    textMessageField.config(highlightbackground="#146604",highlightthickness=3)
     pywhatkit.sendwhatmsg_instantly(phoneNumberFormat,textMessageFormat)
 
 def main():
+    messagebox.showinfo("information", "this is an extension of your whatsApp web account" \
+    " please make sure you have an active account, and all input fields are correctly entered")
     root = tk.Tk()
     root.title("WhatsApp Messenger GUI")
     root.geometry("400x180")
-    messagebox.showinfo("information", "this is an extension of your whatsApp web account" \
-    " please make sure you have an active account, and all input fields are correctly entered")
     root.resizable(True, False)
     root.configure(bg="black")
     phoneNumberPrompt = tk.Label(root,text="Enter Phone Number Below",
@@ -68,11 +76,11 @@ def main():
     textMessageField.pack(fill="x", expand=True)
     textMessageField.insert(0,"Hello,John Doe!")
     clearButton = tk.Button(root, text = "Clear all Fields",
-                            command=lambda: clearFields(phoneNumberField,textMessageField))
+                             command=lambda: clearFields(phoneNumberField,textMessageField))
     clearButton.pack(fill="x", expand=True)
     sendButton = tk.Button(root, text="Send",
                            command=lambda: MyClick(phoneNumberField, textMessageField))
-    sendButton.pack(fill="x", expand=True)
+    sendButton.pack(fill="x",expand=True)
     root.mainloop()
 
 if __name__ == "__main__":
